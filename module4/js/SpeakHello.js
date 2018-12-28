afa1568022c83ee99e8711be4bd73d4c66f36d79
Dong-Myeong Seo <doseo@oradev.oraclecorp.com>
2018-12-27 18:01:41 -0700

    try again

--- module4/js/SpeakHello.js
+++ module4/js/SpeakHello.js
@@ -15,6 +15,7 @@ var speakWord = "Hello";
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
+document.getElementById("name")
 helloSpeaker.speak = function (name){
 	console.log(speakWord + " " + name);
 }
